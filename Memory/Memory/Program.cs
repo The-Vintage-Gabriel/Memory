87ad9a1d0760244774b9a593fbeffb0906320155
f.stocker1 <f.stocker1@htlkrems.at>
2026-06-02 14:43:22 +0200

    function definiert

--- Memory/Memory/Program.cs
+++ Memory/Memory/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string[] a = new String["Schmetterling", "Glühbirne", "Kaffeetasse", "Regenbogen", "Windmühle", "Sternenhimmel"];
+string[] b = new String["Taschenlampe", "Sanduhr", "Wolkenbruch", "Federkissen", "Ziegelstein", "Wasserfall"];
+string[] c = new String["Fetzen", "Glitzerstaub", "Fahrradklingel", "Murmeltier", "Papierflieger", "Dinosauria"];
+string[] d = new String["Fahrradklingel", "Glitzerstaub", "Taschenlampe", "Fetzen", "Glühbirne", "Sanduhr"];
+string[] e = new String["Glitzerstaub", "Wolkenbruch", "Kaffeetasse", "Murmeltier", "Federkissen", "Regenbogen"];
+string[] f = new String["Windmühle", "Ziegelstein", "Papierflieger", "Sternenhimmel", "Wasserfall", "Dinosauria"];
 
-Console.WriteLine("Hello, World!");
+string[,] field = {{"a",  "b", "c"},
+                   {"d",  "e", "f"}, 
+                   {"g",  "h", "i"}} 
+string s = field[0,1]
+
+static void print()
+{
+    
+}
+
+static string[,] rndfield()
+{
+    
+}
+
+static string[,] turned()
+{
+    
+}
+
+static string[,] testifturned()
+{
+    
+}
+
